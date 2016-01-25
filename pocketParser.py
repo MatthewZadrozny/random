3fa889390a87e2ada167b34574bccc70d8c883e3
Matthew <m.zadrozny@gmail.com>
2016-01-25 10:49:58 -0500

    Fixed indents

--- pocketParser.py
+++ pocketParser.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*
-#pocketParser.py
-#Author: m.zadrozny@gmail.com
-#Date: 2014-01-16
-#Requests getpocket.com html data export and dumps it into a 
+# pocketParser.py
+# Author: m.zadrozny[at]gmail
+# Date: 2014-01-16
+# Requests getpocket.com html data export and dumps it into a 
 #  ` -delimitted text file
-#Data should be marked as UTF-8 when imported into spreadsheet
+# Data should be marked as UTF-8 when imported into spreadsheet
 
 import datetime, re, Tkinter, tkFileDialog
 
@@ -21,17 +21,17 @@ pat_time = re.compile('time_added=".+?"')
 status = 'Unread'
 
 with open(s) as i, open('pocket_output.txt', 'w') as o: 
-	for line in i: 
-		if '<h1>Read Archive</h1>' in line: status = 'Read'
-		elif '<li>' in line: 
-			url = pat_url.findall(line)[0][9:-1]
-			time = pat_time.findall(line)[0][12:-1]
-			title = pat_title.sub('', line).strip().strip('\n')
-			try: tags = pat_tags.findall(line)[0][6:-1]
-			except: tags = 'no_tag' #older untagged articles seem to have been saved as ''
+    for line in i: 
+        if '<h1>Read Archive</h1>' in line: status = 'Read'
+        elif '<li>' in line: 
+            url = pat_url.findall(line)[0][9:-1]
+            time = pat_time.findall(line)[0][12:-1]
+            title = pat_title.sub('', line).strip().strip('\n')
+            try: tags = pat_tags.findall(line)[0][6:-1]
+            except: tags = 'no_tag' #older untagged articles seem to have been saved as ''
 
-			time = (datetime.datetime.fromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S'))
+            time = (datetime.datetime.fromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S'))
 
-			entry = '`'.join([title, time, tags, url, status, '\n'])
+            entry = '`'.join([title, time, tags, url, status, '\n'])
 
-			o.write(entry)
+            o.write(entry)
